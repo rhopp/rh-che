3b1d3889a224692a270e766650cf9589a63ca5e4
Radim Hopp <rhopp@redhat.com>
2019-12-08 19:44:21 +0100

    fixup#9

--- .ci/cico_rhche_prcheck.sh
+++ .ci/cico_rhche_prcheck.sh
@@ -40,6 +40,13 @@ set -x
 nmcli > nmclioutput
 cat nmclioutput
 
+firewall-cmd --permanent --new-zone dockerc
+firewall-cmd --permanent --zone dockerc --add-source 172.17.0.0/16
+firewall-cmd --permanent --zone dockerc --add-port 8443/tcp
+firewall-cmd --permanent --zone dockerc --add-port 53/udp
+firewall-cmd --permanent --zone dockerc --add-port 8053/udp
+firewall-cmd --reload
+
 
 LOCAL_IP_ADDRESS=$(ip a show | grep -e "scope.*eth0" | grep -v ':' | cut -d/ -f1 | awk 'NR==1{print $2}')
 echo $LOCAL_IP_ADDRESS
