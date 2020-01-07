65607fdefb591ff33451ee3c265caebdc86b493b
Radim Hopp <rhopp@redhat.com>
2020-01-07 13:07:21 +0100

    Trying minikube - fixup11

--- .ci/cico_rhche_prcheck.sh
+++ .ci/cico_rhche_prcheck.sh
@@ -127,7 +127,7 @@ CHE_ROUTE=$(oc get route che --template='{{ .spec.host }}')
 mkdir report
 REPORT_FOLDER=$(pwd)/report
 set +e
-docker run --shm-size=256m -v $REPORT_FOLDER:/root/e2e/report:Z -e TS_SELENIUM_BASE_URL="http://$CHE_ROUTE" -e TS_SELENIUM_MULTIUSER="true" -e TS_SELENIUM_USERNAME="admin" -e TS_SELENIUM_PASSWORD="admin" eclipse/che-e2e:nightly
+docker run --shm-size=256m -v $REPORT_FOLDER:/tmp/e2e/report:Z -e TS_SELENIUM_BASE_URL="http://$CHE_ROUTE" -e TS_SELENIUM_MULTIUSER="true" -e TS_SELENIUM_USERNAME="admin" -e TS_SELENIUM_PASSWORD="admin" eclipse/che-e2e:nightly
 set -e
 archiveArtifacts1
 
